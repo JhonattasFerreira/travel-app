3ef05d450cd4467101b98d92638627b9292670a3
Jhonattas Ferreira <jfs120@gmail.com>
2021-03-27 18:17:03 -0300

    feat: Import the functions

--- src/client/index.js
+++ src/client/index.js
@@ -3,9 +3,10 @@ import './style/form.scss';
 import './style/content.scss';
 
 import './js/formHandler';
-import {isValidInputs} from './js/util';
+import {isValidInputs, checkDateForecast} from './js/util';
+import {updateBasicInformationUi, updateForecastUi, updateImageUi, updateCountryInformation, cleanUi, setErrorMessage} from './js/uiElements';
 
 
 console.log('hello World');
 
-export {isValidInputs};
+export {isValidInputs, checkDateForecast, updateBasicInformationUi, updateForecastUi, updateImageUi, updateCountryInformation, cleanUi, setErrorMessage};
