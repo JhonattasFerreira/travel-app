888581540b9c35e04c13704dab2db53608f029a1
Jhonattas Ferreira <jfs120@gmail.com>
2021-03-27 03:43:50 -0300

    chore: Add the config of injectClient output on dev server

--- webpack.dev.js
+++ webpack.dev.js
@@ -14,7 +14,8 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 9000
+    port: 9000,
+    injectClient: false
   },
   module: {
     rules: [
